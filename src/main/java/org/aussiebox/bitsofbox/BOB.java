64565bb9d7bf08a3bea380139568e477246840b7
AussieBox <boxofaustralians@gmail.com>
2026-01-04 20:41:44 +1000

    Fix Fluidity recipes (AGAIN????) + Make Shimmer Powder obtainable by clicking the World Border (in theory)

--- src/main/java/org/aussiebox/bitsofbox/BOB.java
+++ src/main/java/org/aussiebox/bitsofbox/BOB.java
@@ -1,7 +1,12 @@
 package org.aussiebox.bitsofbox;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.gen.treedecorator.TreeDecoratorType;
 import org.aussiebox.bitsofbox.attach.ModAttachmentTypes;
 import org.aussiebox.bitsofbox.block.ModBlocks;
@@ -35,6 +40,22 @@ public class BOB implements ModInitializer {
         ModDataComponentTypes.init();
 
         DragonflameCactusGeneration.generateCacti();
+
+        UseItemCallback.EVENT.register((player, world, hand) -> {
+
+            if (!(world instanceof ServerWorld serverWorld)) return TypedActionResult.pass(player.getMainHandStack());
+
+            HitResult raycast = player.raycast(player.getBlockInteractionRange(), 0, false);
+            if (!serverWorld.getWorldBorder().contains(raycast.getPos())) {
+
+                ItemStack stack = new ItemStack(ModItems.SHIMMER_POWDER);
+                player.giveItemStack(stack);
+
+                return TypedActionResult.success(player.getMainHandStack());
+            }
+
+            return TypedActionResult.pass(player.getMainHandStack());
+        });
 
     }
 }
